3c5fb59466e8ed311e9234746eefee9117d3c018
fengchunsgit <1055688803@qq.com>
2019-07-21 22:05:54 +0800

    exception

--- src/main/java/com/ne/no/javamail/hello/MailService.java
+++ src/main/java/com/ne/no/javamail/hello/MailService.java
@@ -1,5 +1,7 @@
 package com.ne.no.javamail.hello;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.FileSystemResource;
@@ -14,6 +16,8 @@ import java.io.File;
 
 @Service
 public class MailService {
+
+    private final Logger logger= LoggerFactory.getLogger(this.getClass());
 
     @Value("${spring.mail.username}")
     private String from;
@@ -38,42 +42,61 @@ public class MailService {
         mailSender.send(message);
     }
 
-    public void sendHtmlMail(String to,String subject,String content) throws MessagingException {
+    public void sendHtmlMail(String to,String subject,String content){
         MimeMessage message =mailSender.createMimeMessage();
-        MimeMessageHelper helper=new MimeMessageHelper(message,true);
-        helper.setFrom(from);
-        helper.setTo(to);
-        helper.setSubject(subject);
-        helper.setText(content,true);
-        mailSender.send(message);
+        MimeMessageHelper helper= null;
+        try {
+            helper = new MimeMessageHelper(message,true);
+            helper.setFrom(from);
+            helper.setTo(to);
+            helper.setSubject(subject);
+            helper.setText(content,true);
+            mailSender.send(message);
+        } catch (MessagingException e) {
+            e.printStackTrace();
+        }
+
     }
 
-    public void sendAttachmentsMail(String to,String subject,String content,String filepath) throws MessagingException {
+    public void sendAttachmentsMail(String to,String subject,String content,String filepath){
         MimeMessage message=mailSender.createMimeMessage();
-        MimeMessageHelper helper=new MimeMessageHelper(message,true);
+        MimeMessageHelper helper= null;
+        try {
+            helper = new MimeMessageHelper(message,true);
+            helper.setFrom(from);
+            helper.setTo(to);
+            helper.setSubject(subject);
+            helper.setText(content,true);
 
-        helper.setFrom(from);
-        helper.setTo(to);
-        helper.setSubject(subject);
-        helper.setText(content,true);
+            FileSystemResource file=new FileSystemResource(new File(filepath));
+            String fileName=file.getFilename();
+            helper.addAttachment(fileName,file);
+            mailSender.send(message);
+        } catch (MessagingException e) {
+            e.printStackTrace();
+        }
 
-        FileSystemResource file=new FileSystemResource(new File(filepath));
-        String fileName=file.getFilename();
-        helper.addAttachment(fileName,file);
-        mailSender.send(message);
     }
 
-    public void sendInlineResourceMail(String to,String subject,String content,String rscPath,String rscId) throws MessagingException {
+    public void sendInlineResourceMail(String to,String subject,String content,String rscPath,String rscId){
+        logger.info("发送静态邮件开始：{}，{}，{}，{}，{}",to,subject,content,rscPath,rscId);
         MimeMessage message=mailSender.createMimeMessage();
-        MimeMessageHelper helper=new MimeMessageHelper(message,true);
-        helper.setFrom(from);
-        helper.setTo(to);
-        helper.setSubject(subject);
-        helper.setText(content,true);
+        MimeMessageHelper helper= null;
+        try {
+            helper = new MimeMessageHelper(message,true);
+            helper.setFrom(from);
+            helper.setTo(to);
+            helper.setSubject(subject);
+            helper.setText(content,true);
 
-        FileSystemResource res=new FileSystemResource(new File(rscPath));
-        helper.addInline(rscId,res);
-        mailSender.send(message);
+            FileSystemResource res=new FileSystemResource(new File(rscPath));
+            helper.addInline(rscId,res);
+            mailSender.send(message);
+            logger.info("发送静态图片邮件成功！");
+        } catch (MessagingException e) {
+            logger.error("发送静态邮件异常：",e);
+        }
+
     }
 
 }
