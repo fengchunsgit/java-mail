003140f2d5bc542a81367b795c6e257aaaf6463b
fengchunsgit <1055688803@qq.com>
2019-07-21 18:29:08 +0800

    init

---
+++ src/main/java/com/ne/no/javamail/hello/HelloService.java
@@ -0,0 +1,11 @@
+package com.ne.no.javamail.hello;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class HelloService {
+    public void sayHello(){
+        System.out.println("hello");
+    }
+
+}
---
+++ src/test/java/com/ne/no/javamail/service/HelloTest.java
@@ -0,0 +1,23 @@
+package com.ne.no.javamail.service;
+
+
+
+import com.ne.no.javamail.hello.HelloService;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import javax.annotation.Resource;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class HelloTest {
+    @Resource
+    HelloService helloService;
+
+    @Test
+    public void sayHelloTest(){
+        helloService.sayHello();
+    }
+}
